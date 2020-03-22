e01a388a5ab3e922dda5be8c16a27e64477815fd
Enrique Hernandez <hernandeze2@xavier.edu>
2020-03-22 10:34:04 -0400

    send and receive object

--- client.py
+++ client.py
@@ -1,17 +1,32 @@
 import socket
+import pickle
+
+HEADERSIZE = 10
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(('localhost', 1235))
+clientSocket.connect(('localhost', 1234))
 
 #maintain connection
 while True:
     #buffer through message
-    full_msg = ''
+    full_msg = b''
+    new_msg = True
     while True:
-        msg = clientSocket.recv(8)
-        if len(msg) <= 0:
-            break
-        full_msg += msg.decode("utf-8")
+        msg = clientSocket.recv(16)
+        if new_msg:
+            print("new msg len:", msg[:HEADERSIZE])
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
 
-    if len(full_msg) > 0:
-        print(full_msg)
+        print(f'full message length: {msglen}')
+
+        full_msg += msg
+
+        print(len(full_msg))
+
+        if len(full_msg) - HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            print(pickle.loads(full_msg[HEADERSIZE:]))
+            new_msg = True
+            full_msg = b''
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import socket
+import pickle
+import time
 
 HEADERSIZE = 10
 
 #create socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #bind socket to localhost:1235
-serverSocket.bind(('localhost', 1235))
+serverSocket.bind(('localhost', 1234))
 #max number of ques available
 serverSocket.listen(5)
 
@@ -16,9 +18,10 @@ while True:
     print(f'connection from {address} has been establihed')
 
     #produces the length of message with 10 characters before the message
-    msg = "Hellow World from the server"
-    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    d = {1: "hi", 2: "there"}
+    msg = pickle.dumps(d)
+    msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
+    print(msg)
 
     #send message
-    clientsocket.send(bytes(msg, "utf-8"))
-    clientsocket.close()
+    clientsocket.send(msg)
